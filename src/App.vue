52f27e9c13ac476940e75032c7cb2c01173aa97a
Sonia Reuter <47145589+Sreuter29@users.noreply.github.com>
2021-03-02 11:33:46 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+
+<!-- on introduit le header dans le template -->
+    <Header/>
+
+<!-- on introduit la Nav dans le template -->
+    <Nav/>
+
+<!-- on introduit le test focus -->
+    <focus/>
+
+<!-- on introduit le main dans le template -->
+    <Main/>
+
+<!-- on introduit le footer dans le template -->
+  <!-- <Footer/> -->
+
+  </div>
+</template>
+
+<script>
+
+// on import l'ensemble des composants utilisés par app.vue
+import Header from './components/header.vue'
+import Footer from './components/footer.vue'
+import Main from './components/main.vue'
+import Nav from './components/nav.vue'
+import state from './state.js'
+import focus from './components/focus.vue'
+
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+    Main,
+    Footer,
+    Nav,
+    focus
+  },
+
+// ce sont les datas du state qui sont utilisés dans app.vue
+  data() {
+    return {
+      state        
+      
+    };
+  },
+  created() {
+    this.state.profil = 'adult'
+    
+  },
+  watch: {
+    'state.profil': function(profile) {
+      document.body.style.backgroundImage = `url("background/fond-${profile}.jpg")`;
+    }
+  },
+}
+</script>
+
+<style lang="less">
+// import des deux feuilles de style
+  @import "./assets/style/normalize.css";
+  @import "./assets/style/style.css";
+</style>
---
+++ src/channels.js
@@ -0,0 +1,18 @@
+export const channels = 
+    [
+        { channel: 'tf1.png', prime: 'tf1_prime.jpg', focus: 'Mychannels_1'},
+        { channel: 'france22.png', prime: 'france22_prime.jpg', focus: 'Records_1'},
+        { channel: 'france3.png', prime: 'france3_prime.jpg', focus: 'Apps_1'},
+        { channel: 'france5.png', prime: 'france5_prime.jpg', focus: 'Foru_1'},
+        { channel: 'm6.png', prime: 'm6_prime.jpg', focus: 'Alaune_1'},
+        { channel: 'Animaux.png', prime: 'Animaux_prime.jpg', focus: 'Mychannels_2'},
+        { channel: 'arte.png', prime: 'arte_prime.jpg', focus: 'Records_2'},
+        { channel: 'c8.png', prime: 'c8_prime.jpg', focus: 'Apps_2'},
+        { channel: 'tfx.png', prime: 'tfx_prime.jpg', focus: 'Foru_2'},
+        { channel: 'escales.png', prime: 'escales_prime.jpg', focus: 'Alaune_2'},
+        { channel: 'france4.png', prime: 'france4_prime.jpg', focus: 'Mychannels_3'},
+        { channel: 'bfmtv.png', prime: 'bfmtv_prime.jpg', focus: 'Records_3'},
+        { channel: 'itele.png', prime: 'itele_prime.jpg', focus: 'Apps_3'},
+        { channel: 'rmc.png', prime: 'rmc_prime.jpg', focus: 'Foru_3'},
+        { channel: 'TVBREIZH.png', prime: 'TVBREIZH_prime.jpg', focus: 'Alaune_3'},
+]
